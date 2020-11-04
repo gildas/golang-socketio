5229fa1285da7b6f61c39cc7b1e7b6c24c0e59a6
Gildas Cherruel <gildas@breizh.org>
2020-11-04 19:12:56 +0900

    Added Dial with Namespace

--- client.go
+++ client.go
@@ -1,9 +1,11 @@
 package gosocketio
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 
+	"github.com/gildas/golang-socketio/protocol"
 	"github.com/gildas/golang-socketio/transport"
 )
 
@@ -64,6 +66,41 @@ func Dial(url string, tr transport.Transport) (*Client, error) {
 }
 
 /**
+connect to host and initialise socket.io protocol
+
+The correct ws protocol url example:
+ws://myserver.com/socket.io/?EIO=3&transport=websocket
+
+You can use GetUrlByHost for generating correct url
+*/
+func DialWithNamespace(url string, namespace string, tr transport.Transport) (*Client, error) {
+	c := &Client{}
+	c.initChannel()
+	c.initMethods()
+
+	var err error
+	c.conn, err = tr.Connect(url)
+	if err != nil {
+		return nil, err
+	}
+
+	message := fmt.Sprintf("4%d%s", protocol.MessageTypeOpen, namespace)
+	err = c.conn.WriteMessage(message)
+	if err != nil {
+		return nil, err
+	}
+
+	go inLoop(&c.Channel, &c.methods)
+	go outLoop(&c.Channel, &c.methods)
+	go pinger(&c.Channel)
+	c.On(OnDisconnection, func(channel *Channel, message interface{}) {
+		c.RedialWithNamespace(url, namespace, tr)
+	})
+
+	return c, nil
+}
+
+/**
 Close client connection
 */
 func (c *Client) Close() {
@@ -71,7 +108,7 @@ func (c *Client) Close() {
 }
 
 /**
-Re
+Redials
 */
 func (c *Client) Redial(url string, tr transport.Transport) {
 	var err error
@@ -82,6 +119,34 @@ func (c *Client) Redial(url string, tr transport.Transport) {
 		case <-ticker.C:
 			c.conn, err = tr.Connect(url)
 			if err == nil {
+				go inLoop(&c.Channel, &c.methods)
+				go outLoop(&c.Channel, &c.methods)
+				go pinger(&c.Channel)
+				return
+			}
+		}
+	}
+}
+
+
+/**
+Redials
+*/
+func (c *Client) RedialWithNamespace(url string, namespace string, tr transport.Transport) {
+	var err error
+	c.initChannel()
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case <-ticker.C:
+			c.conn, err = tr.Connect(url)
+			if err == nil {
+				message := fmt.Sprintf("4%d%s", protocol.MessageTypeOpen, namespace)
+				err = c.conn.WriteMessage(message)
+				if err != nil {
+					continue
+				}
+
 				go inLoop(&c.Channel, &c.methods)
 				go outLoop(&c.Channel, &c.methods)
 				go pinger(&c.Channel)
